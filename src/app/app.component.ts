131a529fd3352a049c1df6b282851d9896818542
Thilina-Upendra <upendravass1996@gmail.com>
2022-09-02 19:34:38 +0530

    Create the basic structure of the Contract manipulation UI part-2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Contract} from "./dto/Contract";
+import {ContractService} from "./service/contract.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'hotel_reservation_frontend';
+export class AppComponent implements OnInit{
+
+
+  contracts: Array<Contract> = [];
+
+  constructor(private contractService: ContractService) {
+  }
+
+  ngOnInit(): void {
+    let contractOne = new Contract("CON001", "H002", new Date(2022,1),new Date(2022,2),25.03);
+    let contractTwo = new Contract("CON002", "H002", new Date(2023,1),new Date(2045,2),15.53);
+    this.contracts.push(contractOne);
+    this.contracts.push(contractTwo);
+  }
+
+
 }
--- src/app/contract/contract.component.ts
+++ src/app/contract/contract.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Contract} from "../dto/Contract";
 
 @Component({
   selector: 'app-contract',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contract.component.scss']
 })
 export class ContractComponent implements OnInit {
+
+  @Input()
+  contract!:Contract;
 
   constructor() { }
 
